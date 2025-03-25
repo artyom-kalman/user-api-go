a1d269d7c360b9d2f6b49ccc78990ae63b767001
artyom-kalman <p.a.kalashnikov@ya.ru>
2025-03-25 17:28:53 +0900

    Add get_user tests

--- internal/app/repository/get_user_test.go
+++ internal/app/repository/get_user_test.go
@@ -1,10 +1,12 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/artyom-kalman/user-api-go/internal/app/users"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,25 +16,41 @@ func TestGetByID(t *testing.T) {
 
 	repo := NewUserRepository(db)
 
-	t.Run("success", func(t *testing.T) {
-		rows := sqlmock.NewRows([]string{"id", "name", "email"}).
-			AddRow(1, "John Doe", "john@example.com")
+	t.Run("found", func(t *testing.T) {
+		rows := sqlmock.NewRows([]string{"id", "email", "password"}).
+			AddRow(1, "john@example.com", "hashed_password")
 
-		mock.ExpectQuery("SELECT id, name, email FROM users WHERE id = ?").
-			WithArgs(1).
+		mock.ExpectQuery("SELECT id, email, password FROM users WHERE id = 1").
 			WillReturnRows(rows)
 
-		user, err := repo.GetByID(1)
+		user, err := repo.GetUserById("1", context.Background())
 		assert.NoError(t, err)
-		assert.Equal(t, &domain.User{ID: 1, Name: "John Doe", Email: "john@example.com"}, user)
+		assert.Equal(t, &users.User{ID: 1, Email: "john@example.com", Password: "hashed_password"}, user)
 	})
 
 	t.Run("not found", func(t *testing.T) {
-		mock.ExpectQuery("SELECT id, name, email FROM users WHERE id = ?").
-			WithArgs(2).
+		mock.ExpectQuery("SELECT id, email, password FROM users WHERE id = 100").
 			WillReturnError(sql.ErrNoRows)
 
-		user, err := repo.GetByID(2)
+		user, err := repo.GetUserById("100", context.Background())
+		assert.Error(t, err)
+		assert.Nil(t, user)
+	})
+
+	t.Run("negative id", func(t *testing.T) {
+		mock.ExpectQuery("SELECT id, email, password FROM users WHERE id = -1").
+			WillReturnError(sql.ErrNoRows)
+
+		user, err := repo.GetUserById("-1", context.Background())
+		assert.Error(t, err)
+		assert.Nil(t, user)
+	})
+
+	t.Run("not int", func(t *testing.T) {
+		mock.ExpectQuery("SELECT id, email, password FROM users WHERE id = 'abc'").
+			WillReturnError(sql.ErrNoRows)
+
+		user, err := repo.GetUserById("abc", context.Background())
 		assert.Error(t, err)
 		assert.Nil(t, user)
 	})
